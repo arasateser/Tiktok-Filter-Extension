c111bf0665868dba09292c976e9088047e50d3c7
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-02-21 17:29:49 +0300

    scripts and icons moved
    Scripts moved into src folder and icons moved into assets folder to maintain understandability

--- popup.js
+++ popup.js
@@ -1,27 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     const messageElement = document.getElementById("message");
+    const maxClicksInput = document.getElementById("maxClicksInput");
+
+    function sendMessageToScript(action, data = {}) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            if (tabs.length === 0) return;
+
+            chrome.tabs.sendMessage(tabs[0].id, { action, ...data }, function (response) {
+                    console.log("sent message:", action);
+            });
+        });
+    }
 
     document.getElementById("viewMoreButton").addEventListener("click", function () {
         messageElement.textContent = "Pressed Load More Pages";
-
-        const maxClicks = parseInt(maxClicksInput.value, 10) || 5;
-
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                files: ["viewMore.js"]
-            }, () => {
-                chrome.tabs.sendMessage(tabs[0].id, { action: "viewMoreButton", maxClicks });
-            });
-        });
+        const maxClicks = parseInt(maxClicksInput.value, 10) || 20;
+        sendMessageToScript("viewMoreButton", { maxClicks });
     });
 
     document.getElementById("killProcessButton").addEventListener("click", function () {
         messageElement.textContent = "Pressed Kill Process Button";
-
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action: "stopViewMore" });
-        })
+        sendMessageToScript("stopViewMore");
     });
 
     document.getElementById("overrideButton").addEventListener("click", function () {
